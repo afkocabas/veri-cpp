8b7d535d3cc114e8c70ac7c2299bd5437eb721c3
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-03-31 16:51:23 +0300

    uart: uart.sv placeholder top level module added.

---
+++ uart/uart.sv
@@ -0,0 +1,15 @@
+import sumermcu_uart_reg_pkg::*;
+
+module uart (
+    input logic clk_i,
+    input logic rstn_i
+);
+
+  always_comb begin : placeholder_comb
+
+  end
+
+  always_ff @(posedge clk_i or negedge rstn_i) begin : placeholder_ff
+
+  end
+endmodule
